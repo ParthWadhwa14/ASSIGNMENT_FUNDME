6dac9347a0cfcae13b05e7f8fdff3413f47e0e43
ParthWadhwa14 <parthwadhwa1980@gmail.com>
2026-02-01 15:43:40 +0530

    first commit

---
+++ .ipynb_checkpoints/working-checkpoint.py
@@ -0,0 +1,26 @@
+from segment_anything import sam_model_registry, SamPredictor
+import matplotlib.pyplot as plt
+import cv2
+
+import pandas as pd
+import numpy as np
+
+df=pd.read_csv('boxes.csv')
+
+# Load image
+image = cv2.imread("input.jpg")
+
+# Choose a model type + checkpoint
+sam = sam_model_registry["vit_h"](checkpoint="sam_vit_h_4b8939.pth")
+predictor = SamPredictor(sam)
+
+# Set image
+predictor.set_image(image)
+
+# Define a bounding box prompt: (x_min, y_min, x_max, y_max)
+box = [100, 50, 400, 350]  # example values
+masks, scores, logits = predictor.predict(box=box)
+
+# Save mask
+mask = masks[0] * 255
+cv2.imwrite("mask.png", mask.astype("uint8"))
